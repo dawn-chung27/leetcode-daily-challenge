bdd17d022864b72f84ea5a956d35e0ab9a88f525
Dawn Chung <dawn.chung27@gmail.com>
2022-07-14 14:03:46 +0900

    350.Intersect two arrays II added

---
+++ Challenges/Easy/350.Intersection of Two Arrays II.js
@@ -0,0 +1,23 @@
+var intersect = function(nums1, nums2) {
+  let result = [];
+  let hashmap = {};
+
+  for (el of nums1) {
+      (hashmap[el]) ? hashmap[el]++ : hashmap[el] = 1;
+  }
+  
+  for (el2 of nums2) {
+    if (hashmap[el2]) {
+      result.push(el2);
+      hashmap[el2]--;
+    }
+  }
+  
+  console.log(hashmap);
+  return result;
+}
+
+const nums1 = [1, 2, 2, 1];
+const nums2 = [2, 2];
+
+console.log(intersect(nums1, nums2));
